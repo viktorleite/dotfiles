fe797b1de7892f4f39ff81a2b26db29c0184128a
Viktor Leite <viktorleite.s@gmail.com>
2025-03-27 22:17:10 -0400

    feat: organizaçao das configuraçoes nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,6 +26,15 @@ vim.opt.foldlevel = 99
 vim.opt.foldenable = true
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Abri lista de diagnostico [Q]uickfix" })
+-- Se <C-\\><C-n> nao funcionar
+-- tentar <C-\><Cn>
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Sair do modo terminal" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Mover o foco para a janela esquerda" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Mover o foco para a janela direita" })
@@ -33,140 +42,248 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Mover o foco para a janela 
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Mover o foco para a janela superior" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Destacar ao extrair (copiar) text.",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {
-    clear = true,
-  }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Destacar ao extrair (copiar) texto.",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {
+		clear = true,
+	}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
-
+-- [[ Instala o lazy.vim plugin manager ]] --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
-end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		error("Erro ao fazer cloning de lazy.nvim:\n" .. out)
+	end
+end ---@diagnostic disable-next-line: undefined-field
 
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configuração e Instalação de plugins ]]
-require('lazy').setup({
- 'tpope/vim-sleuth', -- Detecta tastop e shiftwidth automaticamente
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-  {
-    'nvim-telescope/telescope.nvim'
-    , event = 'VimEnter'
-    , branch = '0.1.x'
-    , dependecies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fsf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      {
-        'nvim-telescope/telescope-ui-select.nvim'
-      },
-      {
-        'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font
-      },
+require("lazy").setup({
+	"tpope/vim-sleuth", -- Detecta tastop e shiftwidth automaticamente
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+	-- [[ ./lua/plugins/ ]] --
+	require("plugins.nvim-treesitter"),
+	require("plugins.neo-tree"),
+	require("plugins.lint"),
+	require("plugins.gitsigns"),
+	require("plugins.tokyonight"),
+	require("plugins.debug"),
+	require("plugins.autopairs"),
+	require("plugins.telescope"),
+	require("plugins.autoformat"),
+	require("plugins.gitblame"),
+	-- [[ Plugins LSP ]] --
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{
+					path = "${3rd}/luv/library",
+					words = { "vim%.uv" },
+				},
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = {} },
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-      config = function()
-        require('telescope').setup {
-          extensions = {
-            ['uiselect'] = {
-              require('telescope.themes').get_dropdown(),
-            },
-          },
-        }
+			{ "j-hui/fidget.nvim", opts = {} },
 
-        pcall(require('telescope').load_extension, 'fzf')
-        pcall(require('telescope').load_extension, 'ui-select')
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			--
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
 
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
-        local builtin = require 'telescope.builtin'
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-        -- Slightly advanced example of overriding default behavior and theme
-        vim.keymap.set('n', '<leader>/', function()
-          -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-          builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = true,
-          })
-        end, { desc = '[/] Fuzzily search in current buffer'} )
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
-        vim.keymap.set('n', '<leader>s/', function()
-          builtin.live_grep {
-            grep_open_files = true,
-            prompt_title = 'Live Grep in Open Files',
-          }
-        end, { desc = '[S]earch [/] in Open Files' })
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
-        -- Shortcut for searching your Neovim configuration files
-        vim.keymap.set('n', '<leader>sn', function()
-          builtin.find_files { cwd = vim.fn.stdpath 'config' }
-        end, { desc = '[S]earch [N]eovim files' })
-      end,
-    },
-  },
-  -- [[ END Telescope ]]
-  -- [[ Git Blame ]]
-  {
-    "f-person/git-blame.nvim",
-    enabled = true,
-    lazy = false,
-    priority = 7,
-  },
+					map(
+						"<leader>ws",
+						require("telescope.builtin").lsp_dynamic_workspace_symbols,
+						"[W]orkspace [S]ymbols"
+					)
 
-  -- [[ ./lua/plugins/ ]] --
-  require("plugins.neo-tree"),
-},
-{
-    ui = {
-      icons = vim.g.have_nerd_font and {} or {
-          cmd = '⌘',
-          config = '🛠',
-          event = '📅',
-          ft = '📂',
-          init = '⚙',
-          keys = '🗝',
-          plugin = '🔌',
-          runtime = '💻',
-          require = '🌙',
-          source = '📄',
-          start = '🚀',
-          task = '📌',
-          lazy = '💤 ',
-      }
-    },
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+					local function client_supports_method(client, method, bufnr)
+						if vim.fn.has("nvim-0.11") == 1 then
+							return client:supports_method(method, bufnr)
+						else
+							return client.supports_method(method, { bufnr = bufnr })
+						end
+					end
+
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if
+						client
+						and client_supports_method(
+							client,
+							vim.lsp.protocol.Methods.textDocument_documentHighlight,
+							event.buf
+						)
+					then
+						local highlight_augroup =
+							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.document_highlight,
+						})
+
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.clear_references,
+						})
+
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
+					end
+
+					if
+						client
+						and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+					then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "[T]oggle Inlay [H]ints")
+					end
+				end,
+			})
+
+			vim.diagnostic.config({
+				severity_sort = true,
+				float = { border = "rounded", source = "if_many" },
+				underline = { severity = vim.diagnostic.severity.ERROR },
+				signs = vim.g.have_nerd_font and {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "󰅚 ",
+						[vim.diagnostic.severity.WARN] = "󰀪 ",
+						[vim.diagnostic.severity.INFO] = "󰋽 ",
+						[vim.diagnostic.severity.HINT] = "󰌶 ",
+					},
+				} or {},
+				virtual_text = {
+					source = "if_many",
+					spacing = 2,
+					format = function(diagnostic)
+						local diagnostic_message = {
+							[vim.diagnostic.severity.ERROR] = diagnostic.message,
+							[vim.diagnostic.severity.WARN] = diagnostic.message,
+							[vim.diagnostic.severity.INFO] = diagnostic.message,
+							[vim.diagnostic.severity.HINT] = diagnostic.message,
+						}
+						return diagnostic_message[diagnostic.severity]
+					end,
+				},
+			})
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			local servers = {
+				lua_ls = {
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				},
+			}
+
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"stylua",
+			})
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				automatic_installation = false,
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+}, {
+	ui = {
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 })
-
-
---
+++ nvim/lua/plugins/gitblame.lua
@@ -0,0 +1,6 @@
+return {
+	"f-person/git-blame.nvim",
+	enabled = true,
+	lazy = false,
+	priority = 7,
+}
